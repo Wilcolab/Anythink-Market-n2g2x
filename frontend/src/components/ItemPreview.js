30e3db4f09efcf8a6ba8a599a87b1ec34b52306b
BurhanuddinRaja <mails4burhan@gmail.com>
2022-09-18 11:38:23 +0530

    The bug is fixed

--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -1,8 +1,10 @@
 import React from "react";
+import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import agent from "../agent";
-import { connect } from "react-redux";
 import { ITEM_FAVORITED, ITEM_UNFAVORITED } from "../constants/actionTypes";
+import placeholderImg from "../imgs/placeholder.png";
+
 
 const mapDispatchToProps = (dispatch) => ({
   favorite: (slug) =>
@@ -36,7 +38,7 @@ const ItemPreview = (props) => {
     >
       <img
         alt="item"
-        src={item.image}
+        src={item.image == "" ? placeholderImg : item.image}
         className="card-img-top item-img"
         style={{ borderRadius: "20px" }}
       />
