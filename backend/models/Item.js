d382a6141561493006b9f7111826cfd86d02e300
BurhanuddinRaja <mails4burhan@gmail.com>
2022-09-18 12:24:36 +0530

    Backend bug fixed

--- backend/models/Item.js
+++ backend/models/Item.js
@@ -2,13 +2,14 @@ var mongoose = require("mongoose");
 var uniqueValidator = require("mongoose-unique-validator");
 var slug = require("slug");
 var User = mongoose.model("User");
+var placeImage = require('../../frontend/public/placeholder.png')
 
 var ItemSchema = new mongoose.Schema(
   {
     slug: { type: String, lowercase: true, unique: true },
     title: String,
     description: String,
-    image: String,
+    image: {type: String, default: placeImage},
     favoritesCount: { type: Number, default: 0 },
     comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }],
     tagList: [{ type: String }],
